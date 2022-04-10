6c0ef6c991252d12689736cbc96e793c077289fc
Lasse Kløvstad <lasse.klovstad@ciber.no>
2022-04-10 15:24:03 +0200

    Add privacy page

--- gata-frontend/src/App.tsx
+++ gata-frontend/src/App.tsx
@@ -1,20 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import {LoginButton} from "./components/LoginButton";
 import {useGetRole} from "./api/role.api";
 import {useAuth0} from "@auth0/auth0-react";
+import {Routes, Route} from "react-router-dom";
+import {Privacy} from "./components/Privacy";
 
 function App() {
     const {getRoles} = useGetRole();
     const {user} = useAuth0();
 
-  return (
-    <div className="App">
-      <LoginButton/>
-      <button onClick={()=>getRoles()}>Fetch roles</button>
-        {user?.email}
-    </div>
-  );
+    return (
+        <div className="App">
+            <LoginButton/>
+            <button onClick={() => getRoles()}>Fetch roles</button>
+            {user?.email}
+            <Routes>
+                <Route path={"privacy"} element={<Privacy/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- gata-frontend/src/index.tsx
+++ gata-frontend/src/index.tsx
@@ -2,18 +2,21 @@ import reportWebVitals from './reportWebVitals';
 import {createRoot} from 'react-dom/client';
 import App from "./App";
 import {Auth0Provider} from "@auth0/auth0-react";
+import {BrowserRouter} from "react-router-dom";
 
 const container = document.getElementById('root');
 if (container) {
     const root = createRoot(container);
-    root.render(<Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN || ""}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ""}
-        audience={process.env.REACT_APP_AUTH0_AUDIENCE}
-        redirectUri={window.location.origin}
-    >
-        <App />
-    </Auth0Provider>);
+    root.render(<BrowserRouter>
+        <Auth0Provider
+            domain={process.env.REACT_APP_AUTH0_DOMAIN || ""}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ""}
+            audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+            redirectUri={window.location.origin}
+        >
+            <App />
+        </Auth0Provider>
+    </BrowserRouter>);
 }
 
 // If you want to start measuring performance in your app, pass a function
