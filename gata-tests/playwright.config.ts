f0740815cc29efd525f6c5e88188b99bdf6afee7
Lasse Kløvstad <lasse.klovstad@ciber.no>
2022-09-24 17:05:51 +0200

    Halla

--- gata-tests/playwright.config.ts
+++ gata-tests/playwright.config.ts
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html"], ["line"]],
   globalSetup: require.resolve("./globalSetup"),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
