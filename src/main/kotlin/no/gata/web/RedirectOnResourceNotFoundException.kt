0f9073e772888fb6ac81bacafadcf08c57708e81
Lasse Kløvstad <lasse.klovstad@ciber.no>
2023-06-13 20:18:57 +0200

    Fix 404 not found on privacy page

--- src/main/kotlin/no/gata/web/RedirectOnResourceNotFoundException.kt
+++ src/main/kotlin/no/gata/web/RedirectOnResourceNotFoundException.kt
@@ -1,12 +1,15 @@
 package no.gata.web
 
 import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @Controller
 class RedirectOnResourceNotFoundException : ErrorController {
     @RequestMapping("/error")
+    @ResponseStatus(HttpStatus.OK)
     fun redirectOnError(): String {
         return "forward:/"
     }
