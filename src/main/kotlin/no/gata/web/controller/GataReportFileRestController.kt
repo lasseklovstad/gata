a87e05592d11e2a49b04c4f502b254deda90aef4
laklov <lasse.klovstad@gmail.com>
2024-01-12 11:50:16 +0100

    Legg til Dto for Report file også

--- src/main/kotlin/no/gata/web/controller/GataReportFileRestController.kt
+++ src/main/kotlin/no/gata/web/controller/GataReportFileRestController.kt
@@ -1,6 +1,7 @@
 package no.gata.web.controller
 
 import no.gata.web.controller.dtoInn.DtoInnGataReportFile
+import no.gata.web.controller.dtoOut.DtoOutGataReportFile
 import no.gata.web.models.GataReportFile
 import no.gata.web.repository.GataReportFileRepository
 import no.gata.web.service.CloudinaryService
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.Optional
 import java.util.UUID
 
 @RestController
@@ -33,17 +33,17 @@ class GataReportFileRestController {
     @PreAuthorize("hasAuthority('member')")
     fun createReportFile(
         @RequestBody body: DtoInnGataReportFile,
-    ): GataReportFile {
+    ): DtoOutGataReportFile {
         val report = gataReportService.getReport(body.reportId)
         val newFile = GataReportFile(id = null, data = body.data, report = report, cloudUrl = null, cloudId = null)
-        return gataReportFileRepository.save(newFile)
+        return DtoOutGataReportFile(gataReportFileRepository.save(newFile))
     }
 
     @PostMapping("cloud")
     @PreAuthorize("hasAuthority('member')")
     fun createCloudFile(
         @RequestBody body: DtoInnGataReportFile,
-    ): GataReportFile {
+    ): DtoOutGataReportFile {
         val report = gataReportService.getReport(body.reportId)
         val cloudFile = cloudinaryService.uploadFile(body.data)
 
@@ -55,14 +55,14 @@ class GataReportFileRestController {
                 cloudId = cloudFile.cloudId,
                 data = null,
             )
-        return gataReportFileRepository.save(newFile)
+        return DtoOutGataReportFile(gataReportFileRepository.save(newFile))
     }
 
     @GetMapping("{fileId}")
     @PreAuthorize("hasAuthority('member')")
     fun getReportFile(
         @PathVariable fileId: String,
-    ): Optional<GataReportFile> {
-        return gataReportFileRepository.findById(UUID.fromString(fileId))
+    ): DtoOutGataReportFile {
+        return gataReportFileRepository.findById(UUID.fromString(fileId)).get().let { DtoOutGataReportFile(it) }
     }
 }
