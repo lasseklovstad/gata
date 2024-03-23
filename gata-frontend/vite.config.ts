8a6e05464fe74baff5931e8a32c636ed5a8ab5e2
Lasse Kløvstad <lasse.klovstad@ciber.no>
2024-03-23 15:20:28 +0100

    Fix

--- gata-frontend/vite.config.ts
+++ gata-frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { remixDevTools } from "remix-development-tools/vite";
 import { defineConfig } from "vite";
