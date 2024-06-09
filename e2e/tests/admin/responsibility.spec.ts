d8cd66e5a59af63b169a441bd70ebd96cb863830
Lasse Kløvstad <lasse.klovstad@ciber.no>
2024-06-09 15:36:43 +0200

    Add test

--- e2e/tests/admin/responsibility.spec.ts
+++ e2e/tests/admin/responsibility.spec.ts
@@ -1,4 +1,7 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+
+import { env } from "e2e/pages/Environment";
+import { addResponsibilityToMember, removeResponsibilityFromMember } from "e2e/utils/responsibilityUtils";
 
 import { ResponsibilityPage } from "../../pages/ResponsibilityPage";
 
@@ -33,6 +36,31 @@ test.describe("Responsibility page", () => {
       const responsibilityPage = new ResponsibilityPage(page);
       await responsibilityPage.goto();
       await responsibilityPage.editResponsibility(preAddedResponsibilities[0].name, "Tim", "Ikke gunstig å ha denne.");
+   });
+
+   test("Should show users responsible this year", async ({ page }) => {
+      const responsibilityPage = new ResponsibilityPage(page);
+      const respName = preAddedResponsibilities[0].name;
+      const { listItem } = responsibilityPage.getResponsibilityListItem(respName);
+      await expect(listItem).toHaveText("Ansvarlig: Ingen");
+
+      await test.step("Add responsibility to member and verify name shows in list", async () => {
+         await addResponsibilityToMember(page, respName, env.memberUsername, "member");
+         await responsibilityPage.goto();
+
+         await expect(listItem).toHaveText("Ansvarlig: " + env.memberUsername);
+      });
+
+      await test.step("Add responsibility to admin and verify both names shows in list", async () => {
+         await addResponsibilityToMember(page, respName, env.memberUsername, "member");
+         await responsibilityPage.goto();
+         await expect(listItem).toHaveText("Ansvarlig: " + [env.memberUsername, env.adminUsername].join(", "));
+      });
+
+      // Cleanup
+
+      await removeResponsibilityFromMember(page, respName, env.memberUsername, "member");
+      await removeResponsibilityFromMember(page, respName, env.adminUsername, "admin");
    });
 
    test("Should delete responsibility", async ({ page }) => {
---
+++ e2e/utils/responsibilityUtils.ts
@@ -0,0 +1,63 @@
+import type { Page } from "@playwright/test";
+
+import { GataHeader } from "e2e/pages/GataHeader";
+import { MemberOverviewPage } from "e2e/pages/MemberOverviewPage";
+import { MemberPage } from "e2e/pages/MemberPage";
+import { ResponsibilityPage } from "e2e/pages/ResponsibilityPage";
+
+export const editResponsibility = async (page: Page, name: string, content: string) => {
+   const memberPage = new MemberPage(page);
+   const header = new GataHeader(page);
+   await page.goto("/");
+   await header.myPageLink.click();
+   await memberPage.goToResponsibilityTab();
+   await memberPage.editResponsibiblity(name, content);
+};
+
+export const removeResponsibility = async (page: Page, responsibilityName: string) => {
+   const responsibilityPage = new ResponsibilityPage(page);
+   await responsibilityPage.goto();
+   await responsibilityPage.deleteResponsibility(responsibilityName);
+};
+
+export const removeResponsibilityFromMember = async (
+   page: Page,
+   responsibilityName: string,
+   username: string,
+   type: "member" | "admin"
+) => {
+   const memberOverviewPage = new MemberOverviewPage(page);
+   const memberPage = new MemberPage(page);
+
+   await memberOverviewPage.goto();
+   await memberOverviewPage.gotoUser(username, type);
+   await memberPage.goToResponsibilityTab();
+   await memberPage.deleteResponsibility(responsibilityName);
+};
+
+export const addResponsibilityToMember = async (
+   page: Page,
+   responsibilitName: string,
+   username: string,
+   type: "member" | "admin"
+) => {
+   const respPage = new ResponsibilityPage(page);
+   await respPage.goto();
+   await respPage.createNewResponsibility(responsibilitName, "Han er kul");
+   await assignResponsibilityToMember(page, username, responsibilitName, type);
+};
+
+export const assignResponsibilityToMember = async (
+   page: Page,
+   username: string,
+   responsibilityName: string,
+   type: "member" | "admin"
+) => {
+   const memberOverviewPage = new MemberOverviewPage(page);
+   const memberPage = new MemberPage(page);
+
+   await memberOverviewPage.goto();
+   await memberOverviewPage.gotoUser(username, type);
+   await memberPage.goToResponsibilityTab();
+   await memberPage.addResponsibility(responsibilityName);
+};
