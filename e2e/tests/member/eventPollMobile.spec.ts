2d92b116b837f5782f18eb7f1781d1b5d8e8ce98
Lasse Kløvstad <lasse.klovstad@ciber.no>
2024-06-22 01:00:09 +0200

    Add date test

--- e2e/tests/member/eventPollMobile.spec.ts
+++ e2e/tests/member/eventPollMobile.spec.ts
@@ -1,4 +1,6 @@
 import { devices, expect } from "@playwright/test";
+import { addDays, formatDate } from "date-fns";
+import { nb } from "date-fns/locale";
 
 import { EventFormPage } from "e2e/pages/EventFormPage";
 import { EventPage } from "e2e/pages/EventPage";
@@ -68,6 +70,38 @@ test.describe("Event polls", () => {
          await eventPollPage.editPoll(name, { isActive: false });
          await expect(memberPage.getByRole("list", { name })).toHaveAccessibleDescription("Avstemningen er avsluttet");
          await eventPollPage.verifyPollListIsDisabled(name, true);
+      });
+   });
+   test.only("Should create poll with multiselect date and verify list on mobile", async ({ memberPage }) => {
+      const eventPage = EventPage(memberPage);
+      const eventPollPage = EventPollPage(memberPage);
+      await eventPage.linkPolls.click();
+      await expect(eventPollPage.mainHeading).toBeVisible();
+
+      const name = "Når skal det skje?";
+      const options = [new Date(), addDays(new Date(), 1), addDays(new Date(), 2)];
+      const optionTexts = options.map((date) => formatDate(date, "dd.MMMM yyyy", { locale: nb }));
+
+      await test.step("Create poll and verify", async () => {
+         await eventPollPage.buttonCreatePoll.click();
+         await eventPollPage.fillForm({ name, type: "Dato", options, canSelectMultiple: true });
+         await eventPollPage.buttonSubmit.click();
+         await eventPollPage.verifyPollList(name, true, optionTexts, ["0", "0", "0"], [false, false, false]);
+         // Should not show table variant
+         await expect(eventPollPage.getPollTableForm(name)).toBeHidden();
+      });
+
+      await test.step("Select option", async () => {
+         await eventPollPage.checkListPollOption(name, true, optionTexts[1], true);
+         await eventPollPage.verifyPollList(name, true, optionTexts, ["0", "1", "0"], [false, true, false]);
+         await eventPollPage.checkListPollOption(name, true, optionTexts[2], true);
+         await eventPollPage.verifyPollList(name, true, optionTexts, ["0", "1", "1"], [false, true, true]);
+      });
+
+      await test.step("Edit poll name", async () => {
+         const newName = "Når skal vi spise kake?";
+         await eventPollPage.editPoll(name, { isActive: true, name: newName });
+         await expect(memberPage.getByRole("list", { name: newName })).toBeVisible();
       });
    });
 });
