4f62fc464ce5a100b395b823da2b75c98060fc31
laklov <lasse.klovstad@gmail.com>
2024-12-10 22:13:59 +0100

    Fix

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,4 @@
 import { type RouteConfig } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
-console.log(await flatRoutes());
-
 export default flatRoutes() satisfies RouteConfig;
--- knip.ts
+++ knip.ts
@@ -3,7 +3,6 @@ import { fileURLToPath } from "url";
 
 import type { RouteConfigEntry } from "@react-router/dev/routes";
 
-import type { KnipConfig } from "knip";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 const appDir = __dirname + "/app";
@@ -26,4 +25,4 @@ export default {
       "app/entry.server.tsx",
       "react-router.config.ts",
    ],
-} satisfies KnipConfig;
+};
