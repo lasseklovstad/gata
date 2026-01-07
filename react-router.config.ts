fa6805551145b57d39c562605df1bff7dd528d12
Lasse Narula <lasse.klovstad@gmail.com>
2026-01-07 08:48:33 +0100

    Fix something more

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,12 @@ import { sentryOnBuildEnd } from "@sentry/react-router";
 export default {
    ssr: true,
    buildEnd: async ({ viteConfig, reactRouterConfig, buildManifest }) => {
-      await sentryOnBuildEnd({ viteConfig, reactRouterConfig, buildManifest });
+      if (process.env.SENTRY_AUTH_TOKEN && process.env.NODE_ENV === "production") {
+         await sentryOnBuildEnd({
+            viteConfig,
+            reactRouterConfig,
+            buildManifest,
+         });
+      }
    },
 } satisfies Config;
