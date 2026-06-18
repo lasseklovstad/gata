f2416c9778c209664fcad46971ddbbad6a510643
laklov <lasse.klovstad@gmail.com>
2026-06-18 20:29:21 +0200

    chore: Apply all future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,13 @@ import { sentryOnBuildEnd } from "@sentry/react-router";
 
 export default {
    ssr: true,
+   future: {
+      v8_middleware: true,
+      v8_passThroughRequests: true,
+      v8_splitRouteModules: true,
+      v8_trailingSlashAwareDataRequests: true,
+      v8_viteEnvironmentApi: true,
+   },
    buildEnd: async ({ viteConfig, reactRouterConfig, buildManifest }) => {
       if (process.env.SENTRY_AUTH_TOKEN && process.env.NODE_ENV === "production") {
          await sentryOnBuildEnd({
